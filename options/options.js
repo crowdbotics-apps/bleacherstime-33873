4a1d40443978783b4bbf1edae5673c1e9655d73c
Crowdbotics <team@crowdbotics.com>
2022-02-27 03:32:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bleacherstime_33873",
+  url: "https://bleacherstime_33873.botics.co",
+  api: "https://bleacherstime_33873.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
